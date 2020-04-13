aea4ee0b9576dddd60f28f05091880b9f45512c8
onezerosix <onezerosix@users.noreply.github.com>
2020-04-12 21:30:57 -0700

    Add lsquare_plot_with_qr and its README picture

---
+++ lsquare_plot_with_qr.m
@@ -0,0 +1,62 @@
+function [x] = lsquare_plot_with_qr(t, y, basis, plot_step)
+% LSQUARE_PLOT_WITH_QR solve Ax = y for x in the least-square sense and
+% plot the least-square fit using QR decomposition.
+%
+% INPUT
+% t & y: m paired input cordinates
+% basis: cell array of n basis function handles that have t as input
+%          these are used for forming A & plotting the least-square fit
+% plot_step: (OPTIONAL) define the step for least-square fit plot,
+%               defaults to 0.01
+% OUTPUT
+% a plot of t & y and a least-square fit function for the data
+% x: least-square fit solution of Ax = y
+
+% validate and possibly default input
+m = length(t);
+assert(m > 0, 'length of t must be > 0')
+assert(length(y) == m, 'length of y should be same as t')
+n = length(basis);
+assert(n > 0, 'length of basis must be > 0')
+if nargin == 3 || plot_step == 0
+    plot_step = 0.01;
+end
+
+% form matrix A
+A = zeros(m,n);
+for i = 1:m
+    for j = 1:n
+        A(i,j) = basis{j}(t(i));
+    end
+end
+
+% find QR decomposition and w & U
+[Q,R] = qr(A);
+w = Q'*y;
+w = w(1:n, :);
+U = R(1:n, :);
+
+% backward subs to solve x of Ux = w because U is upper triangular
+x = zeros(n,1);
+for i = n : -1 : 1
+    other_terms = 0;
+    for j = n : -1 : i+1
+        other_terms = U(i,j)*x(j) + other_terms;
+    end
+    x(i) = (w(i) - other_terms) / U(i,i);
+end
+
+% make the curve from basis functions
+numbers = min(t) : plot_step:max(t);
+curve = x(1) * basis{1}(numbers);
+for i = 2 : n
+    curve = curve + x(i) * basis{i}(numbers);
+end
+
+% plot the given data points and the curve
+scatter(t,y);
+hold on
+plot(numbers,curve);
+hold off
+
+end
